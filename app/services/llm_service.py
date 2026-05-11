5da659840eb5efb2f09a72f7190a723bc1c7b636
Harshit Vaish <vaish@Harshits-MacBook-Air.local>
2026-05-11 18:58:35 +0530

    Initial FastAPI backend with Ollama integration

---
+++ app/services/llm_service.py
@@ -0,0 +1,19 @@
+import ollama
+
+def generate_response(user_message: str):
+
+    response = ollama.chat(
+        model="qwen2.5-coder:7b",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are RepoGraph AI, an intelligent repository analysis assistant."
+            },
+            {
+                "role": "user",
+                "content": user_message
+            }
+        ]
+    )
+
+    return response["message"]["content"]
